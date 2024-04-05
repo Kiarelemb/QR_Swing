5650c1e9c76836cb0da4616c6e5dca3af46a8ed7
Kiarelemb <kiarelemb@outlook.com>
2024-04-06 00:21:41 +0800

    Changes

--- src/swing/qr/kiarelemb/component/assembly/QRCaret.java
+++ src/swing/qr/kiarelemb/component/assembly/QRCaret.java
@@ -19,138 +19,145 @@ import java.awt.image.BufferedImage;
  * @create 2022-11-21 18:41
  **/
 public class QRCaret extends DefaultCaret implements QRComponentUpdate {
-	private int caratHeight;
-	private Color caretColor;
-	private int fontHeight = -1;
-	private String fontName;
-	private int fontSize;
+    private int caratHeight;
+    private Color caretColor;
+    private int fontHeight = -1;
+    private String fontName;
+    private int fontSize;
+    private Rectangle rec;
 
-	public QRCaret() {
-		setBlinkRate(500);
-		componentFresh();
-	}
+    public QRCaret() {
+        setBlinkRate(500);
+        componentFresh();
+    }
 
-	public QRCaret(Font font) {
-		this.fontName = font.getName();
-		this.fontSize = font.getSize();
-		update();
-		setBlinkRate(500);
-		componentFresh();
-	}
+    public QRCaret(Font font) {
+        this.fontName = font.getName();
+        this.fontSize = font.getSize();
+        update();
+        setBlinkRate(500);
+        componentFresh();
+    }
 
-	public void setCaretColor(Color caretColor) {
-		this.caretColor = caretColor;
-	}
+    public int caretHeight() {
+        return this.caratHeight;
+    }
 
-	public int caretHeight() {
-		return this.caratHeight;
-	}
+    /**
+     * 当光标可见时，该 {@link #rec} 总是更新
+     */
+    public Rectangle rectangle() {
+        return rec;
+    }
 
-	public void update() {
-		Font font = QRFontUtils.getFont(this.fontName, this.fontSize);
-		BufferedImage buff = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
-		Rectangle2D bounds;
-		boolean allDisplay = QRFontUtils.fontCanAllDisplay("你", font);
-		if (allDisplay) {
-			bounds = font.getStringBounds("你", buff.createGraphics().getFontRenderContext());
-		} else {
-			bounds = font.getStringBounds("j", buff.createGraphics().getFontRenderContext());
-		}
-		this.fontHeight = QRMathUtils.doubleToInt(bounds.getHeight());
-	}
+    public void setCaretColor(Color caretColor) {
+        this.caretColor = caretColor;
+    }
 
-	/**
-	 * 请及时调用 {@link QRCaret#update()} 方法
-	 *
-	 * @param font 字体
-	 */
-	public QRCaret setFont(Font font) {
-		this.fontName = font.getFontName();
-		this.fontSize = font.getSize();
-		return this;
-	}
+    public void update() {
+        Font font = QRFontUtils.getFont(this.fontName, this.fontSize);
+        BufferedImage buff = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+        Rectangle2D bounds;
+        boolean allDisplay = QRFontUtils.fontCanAllDisplay("你", font);
+        if (allDisplay) {
+            bounds = font.getStringBounds("你", buff.createGraphics().getFontRenderContext());
+        } else {
+            bounds = font.getStringBounds("j", buff.createGraphics().getFontRenderContext());
+        }
+        this.fontHeight = QRMathUtils.doubleToInt(bounds.getHeight());
+    }
 
-	/**
-	 * 请及时调用 {@link QRCaret#update()} 方法
-	 *
-	 * @param fontSize 字体大小
-	 */
-	public QRCaret setFontSize(int fontSize) {
-		this.fontSize = fontSize;
-		return this;
-	}
+    /**
+     * 请及时调用 {@link QRCaret#update()} 方法
+     *
+     * @param font 字体
+     */
+    public QRCaret setFont(Font font) {
+        this.fontName = font.getFontName();
+        this.fontSize = font.getSize();
+        return this;
+    }
 
-	/**
-	 * 请及时调用 {@link QRCaret#update()} 方法
-	 *
-	 * @param fontName 字体名称
-	 */
-	public QRCaret setFontName(String fontName) {
-		this.fontName = fontName;
-		return this;
-	}
+    /**
+     * 请及时调用 {@link QRCaret#update()} 方法
+     *
+     * @param fontSize 字体大小
+     */
+    public QRCaret setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+        return this;
+    }
 
-	@Override
-	protected final synchronized void damage(Rectangle r) {
-		//让黑影仅为光标大小
-		if (r != null) {
-			this.x = r.x;
-			this.y = r.y + this.fontSize / 10;
-			this.width = 2;
-			this.height = this.fontHeight;
-			repaint();
-		}
-	}
+    /**
+     * 请及时调用 {@link QRCaret#update()} 方法
+     *
+     * @param fontName 字体名称
+     */
+    public QRCaret setFontName(String fontName) {
+        this.fontName = fontName;
+        return this;
+    }
 
-	@Override
-	protected void moveCaret(MouseEvent e) {
-		try {
-			super.moveCaret(e);
-		} catch (Exception ex) {
-		}
-	}
+    @Override
+    protected final synchronized void damage(Rectangle r) {
+        //让黑影仅为光标大小
+        if (r != null) {
+            this.x = r.x;
+            this.y = r.y + this.fontSize / 10;
+            this.width = 2;
+            this.height = this.fontHeight;
+            repaint();
+        }
+    }
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		try {
-			super.mousePressed(e);
-		} catch (Exception ex) {
-		}
-	}
+    @Override
+    protected void moveCaret(MouseEvent e) {
+        try {
+            super.moveCaret(e);
+        } catch (Exception ex) {
+        }
+    }
 
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		try {
-			super.mouseEntered(e);
-		} catch (Exception ex) {
-		}
-	}
+    @Override
+    public void mousePressed(MouseEvent e) {
+        try {
+            super.mousePressed(e);
+        } catch (Exception ex) {
+        }
+    }
 
-	@Override
-	public void paint(Graphics g) {
-		if (isVisible()) {
-			JTextComponent comp = getComponent();
-			if (comp == null) {
-				return;
-			}
-			Rectangle r;
-			try {
-				//noinspection deprecation
-				r = comp.modelToView(getDot());
-				if (r == null) {
-					return;
-				}
-			} catch (Exception e) {
-				return;
-			}
-			g.setColor(this.caretColor);
-			this.caratHeight = r.height;
-			g.fillRect(r.x, r.y + this.fontSize / 10, 2, this.fontHeight == -1 ? r.height : this.fontHeight);
-		}
-	}
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        try {
+            super.mouseEntered(e);
+        } catch (Exception ex) {
+        }
+    }
 
-	@Override
-	public void componentFresh() {
-		this.caretColor = QRColorsAndFonts.CARET_COLOR;
-	}
+    @Override
+    public void paint(Graphics g) {
+        if (isVisible()) {
+            JTextComponent comp = getComponent();
+            if (comp == null) {
+                return;
+            }
+            try {
+                //noinspection deprecation
+                rec = comp.modelToView(getDot());
+                if (rec == null) {
+                    return;
+                }
+            } catch (Exception e) {
+                return;
+            }
+            g.setColor(this.caretColor);
+            this.caratHeight = rec.height;
+            g.fillRect(rec.x, rec.y + this.fontSize / 10, 2, this.fontHeight == -1 ? rec.height : this.fontHeight);
+        }
+    }
+
+    @Override
+    public void componentFresh() {
+        this.caretColor = QRColorsAndFonts.CARET_COLOR;
+    }
 }
