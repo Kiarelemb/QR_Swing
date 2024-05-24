85c5ead93f241e761389642226d421d1c4c73ae8
Kiarelemb <kiarelemb@outlook.com>
2024-05-24 08:11:37 +0800

    update

--- src/swing/qr/kiarelemb/component/combination/QRListTabbedPane.java
+++ src/swing/qr/kiarelemb/component/combination/QRListTabbedPane.java
@@ -28,6 +28,7 @@ public class QRListTabbedPane extends QRPanel {
     protected final ArrayList<QRTabbedContentPanel> panels = new ArrayList<>();
     private int maxLength = 0;
     private final QRTabSelectChangedListener tabSelectChangedListener = new QRTabSelectChangedListener();
+    private final AtomicReference<QRTabbedContentPanel> current = new AtomicReference<>();
     private int selectedIndex = -1;
 
     /**
@@ -54,28 +55,14 @@ public class QRListTabbedPane extends QRPanel {
         this.add(this.list.addScrollPane(), listPositionFromBorderLayout);
         this.list.addMouseListener();
 
-        AtomicReference<QRTabbedContentPanel> current = new AtomicReference<>();
         this.list.addMouseListener(QRMouseListener.TYPE.CLICK, e -> {
             selectedIndex = list.getSelectedIndex();
             if (selectedIndex == -1) {
                 return;
             }
-            QRTabbedContentPanel tabbedContentPanel = panels.get(selectedIndex);
-            int beforeIndex = -1;
-            QRTabbedContentPanel before = current.get();
-            if (before != null) {
-                beforeIndex = panels.indexOf(before);
-                this.remove(before);
-            }
-
-            current.set(tabbedContentPanel);
-            this.add(tabbedContentPanel, BorderLayout.CENTER);
-            QRTabSelectEvent tabSelectEvent = new QRTabSelectEvent(beforeIndex, selectedIndex, tabbedContentPanel);
-            tabbedContentPanel.thisTabSelectChangeAction(tabSelectEvent);
-            this.tabSelectChangedListener.tabSelectChangeAction(tabSelectEvent);
-            this.revalidate();
-            this.repaint();
+            setSelectedTab(selectedIndex);
         });
+
         List<String> contents = this.list.contents();
         int height = 0;
         if (!contents.isEmpty()) {
@@ -87,11 +74,12 @@ public class QRListTabbedPane extends QRPanel {
             }
             Rectangle2D bounds = QRFontUtils.getStringBounds(contents.get(0), this.list.getFont());
             height = (int) (bounds.getHeight() * contents.size());
+            setSelectedTab(0);
         }
         this.list.setPreferredSize(new Dimension(maxLength + 20, height));
         this.tabSelectChangedListener.add(e -> tabSelectChangedAction((QRTabSelectEvent) e));
         this.setPreferredSize(0, height);
-        this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        this.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
     }
 
     public QRListTabbedPane(QRList alist, QRTabbedContentPanel[] panels) {
@@ -139,9 +127,7 @@ public class QRListTabbedPane extends QRPanel {
      * @throws IllegalArgumentException 若索引位置超过 {@link QRList} 内容的范围，则抛出此异常
      */
     public void addPanel(int index, QRTabbedContentPanel panel) {
-        if (index < 0 || index > list.getListSize()) {
-            throw new IllegalArgumentException("Illegal index: " + index);
-        }
+        boundCheck(index);
         panels.add(index, panel);
     }
 
@@ -150,6 +136,33 @@ public class QRListTabbedPane extends QRPanel {
      */
     public void addTabSelectChangedAction(QRActionRegister ar) {
         this.tabSelectChangedListener.add(ar);
+    }
+
+    /**
+     * 设置选中的标签页
+     *
+     * @param selectedIndex 选中标签页的索引
+     * @throws IndexOutOfBoundsException 如果传入的索引超出范围，则抛出此异常
+     */
+    public void setSelectedTab(int selectedIndex) {
+        boundCheck(selectedIndex);
+        QRTabbedContentPanel tabbedContentPanel = panels.get(selectedIndex);
+        int beforeIndex = -1;
+        QRTabbedContentPanel before = current.get();
+        if (before != null) {
+            beforeIndex = panels.indexOf(before);
+            this.remove(before);
+        }
+        current.set(tabbedContentPanel);
+        // 更新 list 的选中索引
+        if (list.getSelectedIndex() != selectedIndex) list.setSelectedIndex(selectedIndex);
+        // 更新选重索引
+        this.selectedIndex = selectedIndex;
+        // 更新 tabbedContentPanel 的内容
+        this.add(tabbedContentPanel, BorderLayout.CENTER);
+        QRTabSelectEvent tabSelectEvent = new QRTabSelectEvent(beforeIndex, selectedIndex, tabbedContentPanel);
+        tabbedContentPanel.thisTabSelectChangeAction(tabSelectEvent);
+        this.tabSelectChangedListener.tabSelectChangeAction(tabSelectEvent);
     }
 
     /**
@@ -167,5 +180,11 @@ public class QRListTabbedPane extends QRPanel {
 
     public int selectedIndex() {
         return selectedIndex;
+    }
+
+    private void boundCheck(int index) {
+        if (index < 0 || index > list.getListSize()) {
+            throw new IndexOutOfBoundsException(index);
+        }
     }
 }
